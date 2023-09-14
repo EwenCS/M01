5662113b11e5001b076ec02344c342e960ba2bec
ECAGNON <ECAGNON@lla.edu>
2023-09-14 09:29:55 +0200

    fichierlog heure et date

--- File1.cpp
+++ File1.cpp
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <string>
 #include <fstream>
+using namespace std;
 int main()
 {
 	 float val1;
@@ -10,20 +11,15 @@ int main()
 	 char operateur;
 	 int choix;
 
-	 std::cout<< "choisir:"<<std::endl<< "1-arithmetique"<<std::endl<< "2-trigo"<<std::endl;
+	 cout<< "choisir:"<<endl<< "1-arithmetique"<<endl<< "2-trigo"<<endl;
 
-	 std::cin>> choix;
-
-
-
+	 cin>> choix;
 
 	if (choix==1)
 	{
-
-		std::cout << "saisir l'operation :";
-		std::cin >> val1 >> operateur >> val2;
-		std::cin.get();
-
+		cout << "saisir l'operation :";
+		cin >> val1 >> operateur >> val2;
+		cin.get();
 
 		switch (operateur)
 		{
@@ -41,35 +37,33 @@ int main()
 						break;
 			 default:
 						break;
+
 		}
-
-
-
-
-
 	}
 	if (choix==2)
 	{
-		std::string ftrigo;
-		std::cin>>ftrigo>>val1;
-
+		string ftrigo;
+		cin>>ftrigo>>val1;
 		//if (ftrigo=="sin")
-
-
 	}
 
+		cout << val1 << operateur << val2 << "=" << resultat <<  endl;
+
+		ofstream fichierLog;
+		fichierLog.open("journal.log");
+
+		time_t t=time(0);
+		tm dateHeure=*localtime(&t);
+		fichierLog<<dateHeure.tm_mday<<"/"<<(dateHeure.tm_mon+1)<<"/"<<(dateHeure.tm_year+1900)<<" ";
+		fichierLog<<dateHeure.tm_hour<<":"<<dateHeure.tm_min<<":"<<dateHeure.tm_sec<<'\t';
+		fichierLog << val1 << operateur << val2 << "=" << resultat << endl;
 
 
 
 
+	fichierLog.close();
+ cin.get();
 
-		std::cout << val1 << operateur << val2 << "=" << resultat << std:: endl;
-
-		std::ofstream FichierLog;
-		FichierLog.open("journal.log");
-		FichierLog << val1 << operateur << val2 << "=" << resultat << std::endl;
-		FichierLog.close();
- std::cin.get();
 
 
  return 0;
